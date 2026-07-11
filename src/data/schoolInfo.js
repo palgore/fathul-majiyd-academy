2ecdfa5f3f9a63937a0e6643c581e0d49f62848c
Sani Ismail <sifalgore@gmail.com>
2026-07-11 02:43:14 +0100

    chores: add the project's folder structure structure

---
+++ src/data/schoolInfo.js
@@ -0,0 +1,7 @@
+export const schoolInfo = {
+    name: 'Fathul Majiyd Academy Falgore',
+    motto: 'Inculcating Moral Values Productivity & Empowerment',
+    address: 'Kofar Gari, Opposite Falgore Islamiyya Primary School',
+    phone: [08039410366, 08027025370, 08035751892],
+    levels: ['Nursery', 'Primary', 'Secondary'],
+};
